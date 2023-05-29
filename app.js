5beec20185462f9a60b7da39fd24b468c82be35c
Gourav Ojha <46746385+gouravojha@users.noreply.github.com>
2023-05-29 11:30:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello Dhruv , Deepak and Vikrant Sir'))
 
+app.get('/data', (req, res) => res.json({
+  "name" : "App is working Fine !!!"
+}))
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
